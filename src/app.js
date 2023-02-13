5b6994525fc4940e1a71676655fd316310bc826d
iarmolla <ivan.armolla@gmail.com>
2023-02-13 16:30:40 -0300

    changing cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import cors from 'cors'
 const app = express()
 
 app.use(cors({
-  origin:'http://localhost:3000',  
+  origin:'https://chimerical-mandazi-47f4f8.netlify.app/login',  
 }))
 
 app.use(express.json())
