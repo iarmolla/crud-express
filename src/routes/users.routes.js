b799bb35a06e10968c7db254b81bde08264a0213
iarmolla <ivan.armolla@gmail.com>
2023-02-15 22:32:51 -0300

    adding patch

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,21 +3,23 @@ import {
   getUsers,
   getUser,
   insertUser,
-  updateSalary,
   deleteUser,
   updateUser,
+  update
 } from "../controllers/users.controller.js"
 import { verifyToken } from '../middlewares/index.js'
 const router = Router()
 
 router.get("/users", getUsers)
 
-router.get("/users/:email", getUser)
+router.get("/users/:query", getUser)
 
 router.post("/users", verifyToken, insertUser)
 
 router.delete("/users/:id",verifyToken, deleteUser)
 
 router.put("/users",verifyToken, updateUser)
+
+router.patch("/users",verifyToken,update)
 
 export default router
