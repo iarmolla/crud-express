88bd1840631a509a24106fd318a8bf4b7ef72bd5
iarmolla <ivan.armolla@gmail.com>
2023-02-15 22:33:39 -0300

    removing validation

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -11,11 +11,7 @@ export const verifyToken = async (req, res, next) => {
       "select * from users where id = ?",
       id
     );
-    if (rows[0].type !== 1) {
-      next()
-    } else {
-      res.status(401).json({ error: 'Unauthorized' })
-    }
+    next()
   } catch (error) {
     res.status(404).json({ error: error?.message })
   }
